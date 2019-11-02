d4771e2a02cdc027a99bf3827865724e259dcbd7
shanelonergan <sptlonergan@gmail.com>
2019-11-02 16:19:48 -0400

    added content to main container

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,22 +1,60 @@
 import React, { useState } from 'react';
 
-import { Box, Button, FormField, Heading, Layer, TextInput } from 'grommet';
+import {
+    Box,
+    Button,
+    FormField,
+    Heading,
+    Layer,
+    TextInput,
+    Grid
+} from 'grommet';
 import LoginForm from '../Components/LoginForm';
 
 const HomeContainer = () => {
     return (
-        // <Box
-        //     direction='row'
-        //     align='center'
-        //     justify='between'
-        //     height='100px'
-        //     width='300px'
-        // >
-        //     <Button label='1' fill onClick={() => {}} />
-        //     <Button label='2' fill onClick={() => {}} />
-        //     <Button label='3' fill onClick={() => {}} />
-        // </Box>
-        null
+        <Box pad='small' gap='small' align='center' justify='center'>
+            <Box>
+                <Grid
+                    rows={['flex']}
+                    columns={['small', 'small', 'small']}
+                    gap='small'
+                >
+                    <Button label='shop' fill onClick={() => {}} />
+                    <Button label='sell' fill onClick={() => {}} />
+                    <Button label='learn' fill onClick={() => {}} />
+                </Grid>
+            </Box>
+            <Box>
+                <Box
+                    background='brand'
+                    pad='medium'
+                    width='large'
+                    height='medium'
+                    align='center'
+                    justify='center'
+                >
+                    spotlight
+                </Box>
+            </Box>
+            <Box>
+                <Grid
+                    rows={['flex']}
+                    columns={['small', 'small', 'small']}
+                    gap='small'
+                >
+                    <Box align='center' justify='center'>
+                        Categories
+                    </Box>
+                    <Box align='center' justify='center'>
+                        Categories
+                    </Box>
+                    <Box align='center' justify='center'>
+                        Categories
+                    </Box>
+                </Grid>
+            </Box>
+        </Box>
     );
 };
 
