5e1cd81f6a80fddc14ac92b5e1216656aa5bebfd
shanelonergan <sptlonergan@gmail.com>
2019-11-04 14:39:08 -0500

    added set all listings action in listing actions/reducer

--- src/Redux/actions/listingActions.js
+++ src/Redux/actions/listingActions.js
@@ -3,8 +3,13 @@ const BASE_URL = 'http://localhost:3000';
 const LISTINGS_URL = BASE_URL + '/listings';
 const SPECIFIC_LISTING_URL = id => LISTINGS_URL + '/' + id;
 
-// ==> FETCH <== \\
+// ==> REDUX ACTIONS \\
+const setAllListingsAction = fetchObj => ({
+    type: 'SET_ALL_LISTINGS',
+    payload: fetchObj
+});
 
+// ==> FETCH <== \\
 const createListing = listingObj => dispatch => {
     const config = {
         method: 'POST',
@@ -20,5 +25,21 @@ const createListing = listingObj => dispatch => {
             console.log(listingObj, 'create listing');
         });
 };
+
+const getAllListings = () => dispatch => {
+    const config = {
+        method: 'GET',
+        headers: {
+            Authorization: `bearer ` + localStorage.token
+        }
+    };
+    fetch(LISTINGS_URL, config)
+        .then(res => res.json())
+        .then(listingsObj => {
+            console.log(listingsObj);
+            dispatch(setAllListingsAction(listingsObj));
+        });
+};
+
 
 export { createListing };
--- src/Redux/listingReducer.js
+++ src/Redux/listingReducer.js
@@ -3,6 +3,8 @@ export default (state = {}, { type, payload }) => {
 
     case 'CREATE_LISTING':
         return { ...state, ...payload }
+    case 'SET_ALL_LISTINGS':
+        return {...state, listings: payload}
 
     default:
         return state
