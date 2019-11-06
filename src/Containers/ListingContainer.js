4bacd7abcbe1c83a13ea92cc9f1940740db156fe
shanelonergan <sptlonergan@gmail.com>
2019-11-06 14:09:51 -0500

    stripe modal now works

--- src/Containers/ListingContainer.js
+++ src/Containers/ListingContainer.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
+import StripeCheckout from 'react-stripe-checkout';
 import { getListing } from '../Redux/actions/listingActions';
 import { createCharge } from '../Redux/actions/transactionActions';
-import { Shop } from 'grommet-icons'
+import { Shop } from 'grommet-icons';
 import {
     Box,
     Image,
@@ -15,22 +15,18 @@ import {
 } from 'grommet';
 import { ResizeSpinLoader } from 'react-css-loaders';
 
-
 const ListingContainer = props => {
-
     const dispatch = useDispatch();
-    // var stripe = Stripe('pk_test_LEfFcUQR5pRWI12plUR9V4Rq00MrKBR0Bg');
-
 
     useEffect(() => {
         const urlArr = window.location.href.split('/');
         const listingId = urlArr[4];
         dispatch(getListing(listingId));
-        dispatch(createCharge())
+        dispatch(createCharge());
     }, []);
 
     const listing = useSelector(state => state.listings.currentListing);
-    const transaction = useSelector(state => state.transaction)
+    const transaction = useSelector(state => state.transaction);
 
     const onBuy = () => {
         // console.log('buying')
@@ -38,34 +34,66 @@ const ListingContainer = props => {
         // props.stripe.redirectToCheckout({
         //     sessionId: transaction.id,
         // }).then(result => console.log(result))
+    };
+
+    const onToken = () => {
+        dispatch(createCharge())
     }
 
     return (
         <>
-        <Box size='medium' direction='row'>
-            {listing ? (
-                <>
-                <Box margin='small' responsive={true}>
-                    <Image src='https://images.garmentory.com/images/2574568/large/Railcar-Spikes-X042-Jeans-20190417013220.jpg?1555464745' />
-                </Box>
-                <Box border={{'color':'brand', 'size':'medium'}} margin='small' pad='small' responsive={true} align='center' justify='center'>
-                    <Text color='brand' weight='bold' margin='small'>{listing.brand.name}</Text>
-                    <Text margin='small'>{listing.name}</Text>
-                    <Text margin='small'>wash: {listing.wash.name}</Text>
-                    <Text margin='small'>mill: {listing.mill.name}</Text>
-                    <Text margin='small'>condition: {listing.condition.name}</Text>
-                    <Text margin='small' color='red'>$ {listing.price}</Text>
-            <Button size='medium' fill="horizontal" a11yTitle='purchase this item' color='brand' primary onClick={onBuy} icon={<Shop/>} label='purchase this item'/>
-                </Box>
-                </>
-            ) : (
-                <ResizeSpinLoader/>
-            )}
-        </Box>
-        <Box size='medium'>
-
-        </Box>
-       </>
+            <Box size='medium' direction='row'>
+                {listing ? (
+                    <>
+                        <Box margin='small' responsive={true}>
+                            <Image src='https://images.garmentory.com/images/2574568/large/Railcar-Spikes-X042-Jeans-20190417013220.jpg?1555464745' />
+                        </Box>
+                        <Box
+                            border={{ color: 'brand', size: 'medium' }}
+                            margin='small'
+                            pad='small'
+                            responsive={true}
+                            align='center'
+                            justify='center'
+                        >
+                            <Text color='brand' weight='bold' margin='small'>
+                                {listing.brand.name}
+                            </Text>
+                            <Text margin='small'>{listing.name}</Text>
+                            <Text margin='small'>
+                                wash: {listing.wash.name}
+                            </Text>
+                            <Text margin='small'>
+                                mill: {listing.mill.name}
+                            </Text>
+                            <Text margin='small'>
+                                condition: {listing.condition.name}
+                            </Text>
+                            <Text margin='small' color='red'>
+                                $ {listing.price}
+                            </Text>
+                            <Button
+                                size='medium'
+                                fill='horizontal'
+                                a11yTitle='purchase this item'
+                                color='brand'
+                                primary
+                                onClick={onBuy}
+                                icon={<Shop />}
+                                label='purchase this item'
+                            />
+                            <StripeCheckout
+                                stripeKey='pk_test_LEfFcUQR5pRWI12plUR9V4Rq00MrKBR0Bg'
+                                token={onToken}
+                            />
+                        </Box>
+                    </>
+                ) : (
+                    <ResizeSpinLoader />
+                )}
+            </Box>
+            <Box size='medium'></Box>
+        </>
     );
 };
 
