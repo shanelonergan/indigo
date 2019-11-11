5a43c853d01f0719b0baf14f07bdaaf4c7c33d37
shanelonergan <sptlonergan@gmail.com>
2019-11-11 15:28:47 -0500

    added stack for images and text on home page

--- src/Containers/HomeContainer.js
+++ src/Containers/HomeContainer.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import { useHistory } from "react-router-dom"
+import { useHistory } from 'react-router-dom';
 
-import {
-    Box,
-    Button,
-    Grid
-} from 'grommet';
-
+import { Box, Button, Grid, Image, Stack, Text } from 'grommet';
 
 const HomeContainer = () => {
     // TODO: refactor into one large grid
-    let history = useHistory()
-    const  handleShop = () => {
-        history.push('/listings')
-    }
-    const  handleSell = () => {
-        history.push('/listings/new')
-    }
+    let history = useHistory();
+    const handleShop = () => {
+        history.push('/listings');
+    };
+    const handleSell = () => {
+        history.push('/listings/new');
+    };
+
+    const pantsSVG = () => {
+        return (
+            <svg
+                width='100%'
+                height='100%'
+                viewBox='0 0 32 32'
+                xmlns='http://www.w3.org/2000/svg'
+                xmlnsXlink='http://www.w3.org/1999/xlink'
+            >
+                <path d='indigo/public/icons/jeans.svg' fill='#000' />
+            </svg>
+        );
+    };
 
     return (
         <Box pad='small' gap='small' align='center' justify='center'>
@@ -32,22 +41,31 @@ const HomeContainer = () => {
                         height='xxsmall'
                         justify='center'
                     >
-                        <Button label='shop' fill={true} onClick={handleShop} color='brand' primary/>
-
+                        <Button
+                            label='shop'
+                            fill={true}
+                            onClick={handleShop}
+                            color='brand'
+                            primary
+                        />
                     </Box>
-                    <Box
-                        justify='center'
-                        align='center'
-                        justify='center'
-                    >
-                        <Button label='sell' fill={true} onClick={handleSell} color='brand' primary/>
+                    <Box justify='center' align='center' justify='center'>
+                        <Button
+                            label='sell'
+                            fill={true}
+                            onClick={handleSell}
+                            color='brand'
+                            primary
+                        />
                     </Box>
-                    <Box
-                        justify='center'
-                        align='center'
-                        justify='center'
-                    >
-                        <Button label='learn' fill={true} onClick={() => {}} color='brand' primary/>
+                    <Box justify='center' align='center' justify='center'>
+                        <Button
+                            label='learn'
+                            fill={true}
+                            onClick={() => {}}
+                            color='brand'
+                            primary
+                        />
                     </Box>
                 </Grid>
             </Box>
@@ -70,35 +88,58 @@ const HomeContainer = () => {
                     gap='small'
                     align='stretch'
                 >
-                    <Box
-                        align='center'
-                        justify='center'
-                        background='brand'
-                        height='small'
-                        justify='center'
-                    >
-                        pants
-                    </Box>
-                    <Box
-                        justify='center'
-                        background='brand'
-                        align='center'
-                        justify='center'
-                    >
-                        jackets
-                    </Box>
-                    <Box
-                        justify='center'
-                        background='brand'
-                        align='center'
-                        justify='center'
-                    >
-                        shirts
-                    </Box>
+                    <Stack anchor='center'>
+                        <Box
+                            align='center'
+                            justify='center'
+                            // background='brand'
+                            height='medium'
+                            justify='center'
+                        >
+                            <Image src='https://cache.mrporter.com/variants/images/5983760397903497/in/w2000_q80.jpg' />
+                        </Box>
+                        <Text color='brand' size='xxlarge'>shirts</Text>
+                    </Stack>
+                    <Stack anchor='center'>
+                        <Box
+                            align='center'
+                            justify='center'
+                            // background='brand'
+                            height='medium'
+                            justify='center'
+                        >
+                            <Image src='https://cache.mrporter.com/variants/images/5983760397903497/in/w2000_q80.jpg' />
+                        </Box>
+                        <Text color='brand' size='xxlarge'>shirts</Text>
+                    </Stack>
+                    <Stack anchor='center'>
+                        <Box
+                            align='center'
+                            justify='center'
+                            // background='brand'
+                            height='medium'
+                            justify='center'
+                        >
+                            <Image src='https://cache.mrporter.com/variants/images/5983760397903497/in/w2000_q80.jpg' />
+                        </Box>
+                        <Text color='brand' size='xxlarge'>shirts</Text>
+                    </Stack>
                 </Grid>
             </Box>
             <Box>
-            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+                <div>
+                    Icons made by{' '}
+                    <a
+                        href='https://www.flaticon.com/authors/freepik'
+                        title='Freepik'
+                    >
+                        Freepik
+                    </a>{' '}
+                    from{' '}
+                    <a href='https://www.flaticon.com/' title='Flaticon'>
+                        www.flaticon.com
+                    </a>
+                </div>
             </Box>
         </Box>
     );
